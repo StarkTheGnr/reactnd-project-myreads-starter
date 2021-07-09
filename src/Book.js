7a7d3e2a651025d3af815ad24bd41637c82bb601
StarkTheGnr <lordsomy@hotmail.com>
2021-07-09 20:10:56 +0200

    feat: Allowed multiple authors to be displayed and updated README.md

--- src/Book.js
+++ src/Book.js
@@ -11,23 +11,22 @@ class Book extends Component{
 
 
 		return (<div className="book">
-                          <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                            <div className="book-shelf-changer">
-                              <select value={book.shelf} onChange={ (opt) => book.shelf !== opt.target.value && this.props.changeShelf(book, opt.target.value) } >
-                                <option value="move" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div className="book-title">{book.title}</div>
-                          <div className="book-authors">{book.authors[0]}</div>
-                        </div>)
+              <div className="book-top">
+                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                <div className="book-shelf-changer">
+                  <select value={book.shelf} onChange={ (opt) => book.shelf !== opt.target.value && this.props.changeShelf(book, opt.target.value) } >
+                    <option value="move" disabled>Move to...</option>
+                    <option value="currentlyReading">Currently Reading</option>
+                    <option value="wantToRead">Want to Read</option>
+                    <option value="read">Read</option>
+                    <option value="none">None</option>
+                  </select>
+                </div>
+              </div>
+              <div className="book-title">{book.title}</div>
+              <div className="book-authors">{book.authors.map((a) => (<p>{a}</p>))}</div>
+            </div>)
 	}
-
 }
 
 export default Book
